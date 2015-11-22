e47f33e4c34381287165947dec1a6d1e6d08fe68
Bobby King <king.robertjoseph@gmail.com>
2015-11-22 17:20:21 -0500

    Adds mugatu quotes

---
+++ mugatu-quotes.js
@@ -0,0 +1,16 @@
+var quotes = {
+	'default': [
+		[
+		"SHUT UP! Enough already, ",
+		"! Who cares about Derek Zoolander anyway? The man has only one look, for Christ's sake! Blue Steel? Ferrari? Le Tigra? They're the same face! Doesn't anybody notice this? I feel like I'm taking crazy pills! I invented the piano key necktie, I invented it! What have you done, ",
+		"? You've done nothing! NOTHIIIING! And I will be a monkey's uncle if I let you ruin this for me, because if you can't get the job done, then I will!"
+		],
+		["Obey my dog!"],
+		["",", are you not aware that I get farty and bloated with a foamy latte?"],
+		["They're break dance fighting..."],
+		["As a catepillar becomes a butterfly, so must you become Derelicte!"],
+	],
+	'busy': ["","... So hot right now... ",""]
+};
+
+module.exports = quotes;
