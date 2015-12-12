6ed657fc3b29453d187d1569c093b3f410983a17
Bryan Mytko <bryanmytko@gmail.com>
2015-12-12 17:44:33 -0500

    Match all flavors to available flavors

--- bot_flavor.js
+++ bot_flavor.js
@@ -1,14 +1,17 @@
 var bot_flavor = {
+  "present": "Hello, you're here.",
+  "already_queued": null,
+  "secret_set": null,
   "remove": "Goodbye! :wave:",
+  "empty_queue": null,
+  "attendance_cleared": "Attendance cleared",
+  "attendance_zero": null,
   "greeting": "Mugatu-Bot connected... NOW GET ME A DAMN LATTE...",
   "quotes": [
-    "SHUT UP! Enough already, <user>! Who cares about Derek Zoolander anyway? The man has only one look, for Christ's sake! Blue Steel? Ferrari? Le Tigra? They're the same face! Doesn't anybody notice this? I feel like I'm taking crazy pills! I invented the piano key necktie, I invented it! What have you done, <user>? You've done nothing! NOTHIIIING! And I will be a monkey's uncle if I let you ruin this for me, because if you can't get the job done, then I will!",
-    "Obey my dog!",
-    "<user> are you not aware that I get farty and bloated with a foamy latte?",
     "They're break dance fighting...",
-    "As a catepillar becomes a butterfly, so must you become Derelicte!",
+    "As a catepillar becomes a butterfly...",
+    "That Hansel... So hot right now..."
   ],
-  "busy": "That <user>... So hot right now..."
 };
 
 module.exports = bot_flavor;
