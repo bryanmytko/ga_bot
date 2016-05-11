4037e1e25ae968727716c5ee40053aee41e8974c
Bryan Mytko <bryanmytko@gmail.com>
2016-05-10 22:20:48 -0400

    Add temporary ping/pong on reconnect url

--- bot-functions.js
+++ bot-functions.js
@@ -4,8 +4,12 @@ module.exports = function(bot, taID, adminID){
   const custom_bot = new CustomBot(bot, taID, adminID);
 
   const custom_bot_functions = function(message, cb){
+    console.log(Date() + ": " + message.type);
+
     if(message.type === "hello") custom_bot.greet(message);
     if(message.type === "message") custom_bot.respond(message);
+    // @TODO Change to a set interval 30s timer on connect
+    if(message.type === "reconnect_url") custom_bot.pong();
   };
 
   return custom_bot_functions
