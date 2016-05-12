2d45a0b9a7384b1f37bfe542e1f2c0661300812f
Bryan Mytko <bryanmytko@gmail.com>
2016-05-12 16:02:09 -0400

    Move file parsing & flavor outside custom bot object

--- bot-functions.js
+++ bot-functions.js
@@ -1,7 +1,25 @@
 module.exports = function(bot, taID, adminID){
   var CustomBot = require("./custom-bot").CustomBot;
 
-  const custom_bot = new CustomBot(bot, taID, adminID);
+  var fs = require('fs'),
+      bot_flavor = require('./bot-flavor');
+
+  var present =
+        JSON.parse(fs.readFileSync("./attendance-db.json","utf8")).present,
+      secret =
+        JSON.parse(fs.readFileSync("./attendance-db.json", "utf8")).secret,
+      queue =
+        JSON.parse(fs.readFileSync("./db.json", "utf8")).queue;
+
+  const custom_bot = new CustomBot(
+        bot,
+        taID,
+        adminID,
+        present,
+        secret,
+        queue,
+        bot_flavor
+      );
 
   const custom_bot_functions = function(message, cb){
     if(message.type === "hello") custom_bot.greet(message);
