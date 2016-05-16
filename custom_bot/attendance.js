7ebfbbd9633b8132006c2844b621a9e203d2a7b7
Bryan Mytko <bryanmytko@gmail.com>
2016-05-16 00:12:00 -0400

    Update attendance to keep daily records unique

--- custom_bot/attendance.js
+++ custom_bot/attendance.js
@@ -11,9 +11,14 @@ module.exports = function(CustomBot){
 
   CustomBot.prototype.checkAttendance = function(){
     var self = this;
+    var today = new Date();
 
     db.get(
-      "SELECT * FROM attendance WHERE name = '" + this.user + "' LIMIT 1",
+      "SELECT * FROM attendance WHERE name = '"
+        + this.user
+        + "' AND created_at = '"
+        + today.setHours(0,0,0,0)
+        + "' LIMIT 1",
       function(err, rows){
         if(!rows) self.insertAttendance();
       }
@@ -28,11 +33,16 @@ module.exports = function(CustomBot){
       { user: self.message.user },
       function(data) {
         var name = data.user.real_name || data.user.name;
-        var stmt = db.prepare("INSERT INTO attendance (name) VALUES (?)");
-        stmt.run(data.user.id);
+        var today = new Date();
+        var stmt = db.prepare(
+          "INSERT INTO attendance (real_name, name, created_at) VALUES (?,?,?)"
+        );
+        stmt.run(data.user.name, data.user.id, today.setHours(0,0,0,0));
 
         self.sendAttendanceMessage(
-           name + " " + (self.bot_flavor.present || "You've been marked as present!")
+           name
+             + " "
+             + (self.bot_flavor.present || "You've been marked as present!")
         );
       }
     );
@@ -40,25 +50,29 @@ module.exports = function(CustomBot){
 
   CustomBot.prototype.prettyAttendance = function(){
     var attendance_zero =
-      this.bot_flavor.attendance_zero || "*_Really?! No one is here today?!_*";
+      this.bot_flavor.attendance_zero || "*_Really?! No one is here today?!_*",
+      today = new Date().setHours(0,0,0,0),
+      self = this;
 
-    var presentArray = this.present.map(function(user){
-      return "- " + (user.real_name || user.name);
-    });
-
-    return "*Attendance*\n" +
-      (presentArray.length ? presentArray.join("\n") : attendance_zero);
+    db.get(
+      "SELECT * FROM attendance WHERE created_at = '" + today + "'",
+      function(err, rows){
+        var attendance = rows.map(function(row){
+          row.name; // Need real name!
+        });
+        var str = "*Attendance*\n" + attendance.join("\n");
+        this.bot.sendMessage(this.channel, str);
+      }
+    );
   };
 
   CustomBot.prototype.clearAttendance = function(){
+    db.run("DELETE FROM attendance");
     var response = this.bot_flavor.attendance_cleared || "Attendance cleared";
-    present = [];
     this.bot.sendMessage(this.message.channel, response);
-    this.backupAttendance(this.present, this.secret);
   };
 
   CustomBot.prototype.attendance = function(){
-    this.bot.sendMessage(this.channel, this.prettyAttendance());
   };
 
   CustomBot.prototype.sendQueueMessage = function(msg){
--- db/migration.js
+++ db/migration.js
@@ -2,7 +2,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('db/ga_bot.sqlite3');
 
 db.serialize(function() {
-  db.run("CREATE TABLE attendance (name VARCHAR(255), created_at DATETIME)");
+  db.run("CREATE TABLE attendance (real_name VARCHAR(255), name VARCHAR(255), created_at DATETIME)");
   db.run("CREATE TABLE secret (value VARCHAR(255))");
   db.run("CREATE TABLE queue (name VARCHAR(255))");
 });
