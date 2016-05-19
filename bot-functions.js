6f7d2db4dd7b71cfd918982c412ab50054bd3e96
Bryan Mytko <bryanmytko@gmail.com>
2016-05-19 03:19:04 -0400

    Add Queue module

--- bot-functions.js
+++ bot-functions.js
@@ -1,6 +1,7 @@
 module.exports = function(bot, taID, adminID){
   var CustomBot = require("./custom_bot/custom-bot").CustomBot;
   var Attendance = require("./custom_bot/attendance")(CustomBot);
+  var Queue = require("./custom_bot/queue")(CustomBot);
 
   var fs = require('fs'),
       bot_flavor = require('./custom_bot/bot-flavor');
