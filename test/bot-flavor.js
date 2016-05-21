159d9589b4d928603fcb949757961cb6e0b77ae1
Bryan Mytko <bryanmytko@gmail.com>
2016-05-21 02:37:41 -0400

    Add test flavor file

---
+++ test/bot-flavor.js
@@ -0,0 +1,13 @@
+var bot_flavor = {
+  "present": null,
+  "already_queued": null,
+  "secret_set": null,
+  "remove": null,
+  "empty_queue": null,
+  "attendance_cleared": null,
+  "attendance_zero": null,
+  "greeting": null,
+  "quotes": null
+};
+
+module.exports = bot_flavor;
