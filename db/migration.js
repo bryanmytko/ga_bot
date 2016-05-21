f97183431259cee159d26e13c4eb78f2b81bdd94
Bryan Mytko <bryanmytko@gmail.com>
2016-05-20 20:03:06 -0400

    Add a user_id column to queue

--- db/migration.js
+++ db/migration.js
@@ -4,7 +4,7 @@ var db = new sqlite3.Database('db/ga_bot.sqlite3');
 db.serialize(function() {
   db.run("CREATE TABLE attendance (real_name VARCHAR(255), name VARCHAR(255), created_at DATETIME)");
   db.run("CREATE TABLE secret (value VARCHAR(255))");
-  db.run("CREATE TABLE queue (name VARCHAR(255))");
+  db.run("CREATE TABLE queue (user_id VARCHAR(255), name VARCHAR(255))");
   db.run("INSERT INTO secret (value) VALUES ('default_secret')");
 });
 
