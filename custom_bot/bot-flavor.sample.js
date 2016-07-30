e1bb43a29d50b3aa154bae37da7b411e787e5d27
Bryan Mytko <bryanmytko@gmail.com>
2016-07-30 15:12:08 -0400

    Add bot-flavor-sample

---
+++ custom_bot/bot-flavor.sample.js
@@ -0,0 +1,17 @@
+var bot_flavor = {
+  "present": "Hello, you're here.",
+  "already_queued": null,
+  "secret_set": null,
+  "remove": "You have been removed!",
+  "empty_queue": null,
+  "attendance_cleared": "Attendance cleared",
+  "attendance_zero": null,
+  "greeting": "Bot connected... ♬ ♪ Happy Days Are Here Again ♬ ♪",
+  "quotes": [
+    "Papa, Can You Hear Me?",
+    "You Don’t Bring Me Flowers...",
+    "Don’t Rain on My Parade!"
+  ],
+};
+
+module.exports = bot_flavor;
