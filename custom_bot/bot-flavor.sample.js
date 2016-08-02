e4f1efd82543b8d1a5bbcb09c0511c3ffe39290b
Bryan Mytko <bryanmytko@gmail.com>
2016-08-02 11:36:38 -0400

    Add default values to bot-flavor; Remove default messages in code

--- custom_bot/bot-flavor.sample.js
+++ custom_bot/bot-flavor.sample.js
@@ -1,11 +1,11 @@
 var bot_flavor = {
   "present": "Hello, you're here.",
-  "already_queued": null,
-  "secret_set": null,
+  "already_queued": "You're already in queue",
+  "secret_set": "Secret already set!",
   "remove": "You have been removed!",
-  "empty_queue": null,
+  "empty_queue": "Queue is empty.",
   "attendance_cleared": "Attendance cleared",
-  "attendance_zero": null,
+  "attendance_zero": "No one is here.",
   "greeting": "Bot connected... ♬ ♪ Happy Days Are Here Again ♬ ♪",
   "quotes": [
     "Papa, Can You Hear Me?",
